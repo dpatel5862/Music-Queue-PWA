cb60a3e7565b481a106dbb6900eda7796c99a8bc
U-WIT\mcnamaras2 <mcnamaras2@WITR9ZWCBA.wit.private>
2017-05-16 13:59:09 -0400

    sql

--- WIT_Senior_Project.git/app/Php1.php
+++ WIT_Senior_Project.git/app/Php1.php
@@ -1,5 +1,8 @@
 <?php
-$html = file_get_contents('https://open.spotify.com/track/7As8h8LJTMIritB8QwSmqr'); //get the html returned from the spotify share url
+$Url=$_POST['web'];
+
+
+$html = file_get_contents($Url); //get the html returned from the spotify share url
 
 $music_doc = new DOMDocument(); //creates a new document to place scraped data into
 
@@ -11,14 +14,37 @@ if(!empty($html)){ //check if any html is actually returned
 	libxml_clear_errors(); //remove errors for unusable html
 
 	$music_xpath = new DOMXPath($music_doc);//creates new DOM path to search through
-	//get all the p tags from HTML doc
-	$music_row = $music_xpath->query('//p');
+	//get all the title tags from HTML doc
+	$music_row = $music_xpath->query('//title');
 
-	// store all pulled P tags and echo back
+	// store all pulled title tags and echo back
 	if($music_row->length > 0){
 		foreach($music_row as $row){
 			echo $row->nodeValue . "<br/>";
-    }
-  }
+      $text=$row->textContent;//Changes from DOM object into text
+		}
+	}
 }
-      ?>
+
+$servername = "localhost";//Servername
+$username = "root";//username
+$password = "123";//password for server
+$dbname = "phptest1";//name of database to use on Server
+
+$conn = new mysqli($servername, $username, $password,$dbname);//establishing conection
+//checking connection to server
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+//inserting data into Server
+$sql = "INSERT INTO SpotifyInfo (Spotify1, Spotify2)
+VALUES ('$Url', '$text')";
+//checking if Insert successful
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
