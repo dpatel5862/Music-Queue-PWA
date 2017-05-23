c2d32f3fa1deffada0fc99464ea65a5ed114dfbb
Dhruv Patel <dpatel5862@gmail.com>
2017-05-23 08:19:40 -0400

    UI updates, firebase install

---
+++ WIT_Senior_Project.git/app/scripts/JS/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCOEt7d6RRy9HxSpl1bNSl1NBS9ojOCTbI",
+    authDomain: "music-queue-pwa.firebaseapp.com",
+    databaseURL: "https://music-queue-pwa.firebaseio.com",
+    projectId: "music-queue-pwa",
+    storageBucket: "music-queue-pwa.appspot.com",
+    messagingSenderId: "955128834487"
+  };
+  firebase.initializeApp(config);
