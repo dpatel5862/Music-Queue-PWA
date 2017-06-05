9447cf8beb8025bddf790d55f4d6f3910353536c
Dhruv Patel <dpatel5862@gmail.com>
2017-06-05 16:53:20 -0400

    new changes

---
+++ public/node-client-app/node-client.js
@@ -0,0 +1,16 @@
+var firebase = require(‘firebase’);
+
+firebase.initializeApp({
+ appName: "Music Queue",
+ serviceAccount: "./service-account.json”,
+ authDomain: "musicqueue-pwa.firebaseapp.com”,
+ databaseURL: "https://musicqueue-pwa.firebaseio.com",
+ storageBucket: "musicqueue-pwa.appspot.com"
+ apiKey: "AIzaSyDuYE2a2Wh6yIb6aXplGS2INdjohnphOKE",
+});
+
+var ref = firebase.app().database().ref();
+ref.once(‘value’)
+ .then(function (snap) {
+ console.log(‘snap.val()’, snap.val());
+ });
