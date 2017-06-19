9f3ba6c5969d6ccd313e3bf6eab8c7093d164836
U-WIT\mcnamaras2 <mcnamaras2@WITR9ZWCBA.wit.private>
2017-06-19 12:16:21 -0400

    commit

--- implicit_grant.js
+++ implicit_grant.js
@@ -12,3 +12,22 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 console.log('Listening on 8887');
 app.listen(8887);
+var request = require('request');
+
+var headers = {
+    'Authorization': 'Bearer+access_token'
+};
+var options = {
+    url: 'https://api.spotify.com/v1/users/me/playlists',
+    headers: headers
+};
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+        var url='https://api.spotify.com/v1/users/me/playlists';
+
+    }
+    return document.getElementById('list').setAttribute('src', url);
+}
+
+request(options, callback);
