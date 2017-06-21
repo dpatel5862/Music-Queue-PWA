5c4bc5f8a317ed94217553fce486bb3cc4aec792
Dhruv Patel <dpatel5862@gmail.com>
2017-06-21 14:26:41 -0400

    table added

--- public/scripts/JS/home.js
+++ public/scripts/JS/home.js
@@ -1,6 +1,5 @@
-(function() {
-// Initialize Firebase
-  const config = {
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyCA57_kMWdNztR5nRXoqRu2dTFeSj7Oyzg",
     authDomain: "musicqueue-pwa.firebaseapp.com",
     databaseURL: "https://musicqueue-pwa.firebaseio.com",
@@ -10,18 +9,4 @@
   };
   firebase.initializeApp(config);
 
-var user = firebase.auth().currentUser;
-var name, email, photoUrl, uid, emailVerified;
-
-if (user != null) {
-  name = user.displayName;
-  email = user.email;
-  photoUrl = user.photoURL;
-  emailVerified = user.emailVerified;
-  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-                   // this value to authenticate with your backend server, if
-                   // you have one. Use User.getToken() instead.
-}
-
-}());
 
