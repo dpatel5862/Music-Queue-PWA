7c010c944f11c112c4e263453e8bd96f7b2865b8
Dhruv Patel <dpatel5862@gmail.com>
2017-08-01 14:45:00 -0400

    final changes

--- public/scripts/JS/home.js
+++ public/scripts/JS/home.js
@@ -19,30 +19,30 @@
 
  // Add login event
  btnLogin.addEventListener('click', e => {
- 	//Get Email and Pass
- 	const email = txtEmail.value;
- 	const pass = txtPassword.value;
- 	const auth = firebase.auth();
- 	//Sign In
- 	const promise = auth.signInWithEmailAndPassword(email,pass);
- 	promise.catch(e=> console.log(e.message));
+  //Get Email and Pass
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  //Sign In
+  const promise = auth.signInWithEmailAndPassword(email,pass);
+  promise.catch(e=> console.log(e.message));
 });
 
  // Add signUp event
  btnSignUp.addEventListener('click', e => {
- 	//Get Email and Pass
- 	//TODO: Check for real email
- 	const email = txtEmail.value;
- 	const pass = txtPassword.value;
- 	const auth = firebase.auth();
- 	//Sign In
- 	const promise = auth.createUserWithEmailAndPassword(email,pass);
- 	promise.catch(e => console.log(e.message));
+  //Get Email and Pass
+  //TODO: Check for real email
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  //Sign In
+  const promise = auth.createUserWithEmailAndPassword(email,pass);
+  promise.catch(e => console.log(e.message));
 });
 
 // Add sign out event
  btnLogout.addEventListener('click', e => {
- 	firebase.auth().signOut();
+  firebase.auth().signOut();
   console.log('user signed out');
  });
 
@@ -54,47 +54,49 @@
  const login = document.getElementById('login');
  const spotifyHeader = document.getElementById('spotifyHeader');
  const createHeader = document.getElementById('createHeader');
+ const listDIV = document.getElementById("listDIV")
 
 
 
 //Add realtime listener
 firebase.auth().onAuthStateChanged(firebaseUser => {
-	if (firebaseUser){
-		console.log(firebaseUser);
+if (firebaseUser){
+console.log(firebaseUser);
     //Changes to UI when user is loged in
-		btnLogout.classList.remove('hide'); 
+btnLogout.classList.remove('hide');
     btnLogin.classList.add('hide');
     btnSignUp.classList.add('hide');
     txtEmail.classList.add('hide');
     txtPassword.classList.add('hide');
     //signInHeader.classList.add('hide');
-    joinPartyTable.classList.remove('hide');
-    hostedPartyTable.classList.remove('hide');
+    //joinPartyTable.classList.remove('hide');
+    //hostedPartyTable.classList.remove('hide');
     document.getElementById("intialLogin").className = "col-lg-6";
     name.classList.remove('hide');
     namebutton.classList.remove('hide');
     login.classList.remove('hide');
     spotifyHeader.classList.remove('hide');
     createHeader.classList.remove('hide');
-
-	}
-	else {
-		console.log('not logged in');
-		btnLogout.classList.add('hide');
+    listDIV.classList.remove('hide');
+}
+else {
+console.log('not logged in');
+btnLogout.classList.add('hide');
     btnLogin.classList.remove('hide');
     btnSignUp.classList.remove('hide');
     txtEmail.classList.remove('hide');
     txtPassword.classList.remove('hide');
     //signInHeader.classList.remove('hide');
-    joinPartyTable.classList.add('hide');
-    hostedPartyTable.classList.add('hide');
+    //joinPartyTable.classList.add('hide');
+    //hostedPartyTable.classList.add('hide');
     document.getElementById("intialLogin").className = "col-lg-12";
     name.classList.add('hide');
     namebutton.classList.add('hide');
     login.classList.add('hide');
     spotifyHeader.classList.add('hide');
     createHeader.classList.add('hide');
-	}
+    listDIV.classList.add('hide');
+}
 
 });
 
@@ -118,34 +120,53 @@ firebase.auth().onAuthStateChanged(user => {
             var URI = playlists[k].URI;
             var SpotifyID = playlists[k].SpotifyID;
             //console.log(Name, URI);
-            document.body.appendChild(playlistsList);
+            listDIV.appendChild(playlistsList);
             var li = document.createElement('li');
+            li.setAttribute("class","list-group-item")
             //var anchor = document.createElement('a')
-            li.innerText = (Name + ": " + URI + ": " + SpotifyID)
-            //anchor.setAttribute('href', "https://open.spotify.com/embed?uri=spotify:user:"+SpotifyID+":playlist:"+URI)
-            localStorage.setItem("URI", URI)
-            localStorage.setItem("SpotifyID", SpotifyID)
-            var button = document.createElement("button"); 
-            button.innerHTML = "Join"; 
-            li.appendChild(button); 
-            button.setAttribute("id","joinButton");
-            button.onclick=function(){
-              window.location='playlist_template.html'; 
-              document.getElementById('list').src = "https://open.spotify.com/embed?uri=spotify:user:"+SpotifyID+":playlist:"+URI 
-            }
+            li.innerText = ("Playlist Name: " + Name + "  " + "Creator: " + SpotifyID + "  ")
+            var srcloc= "https://open.spotify.com/embed?uri=spotify:user:"+ SpotifyID +":playlist:"+URI
+            console.log(srcloc)
+            var button = document.createElement("button")
+            button.innerHTML = "Join"
+            li.appendChild(button)
+            button.setAttribute("class","btn btn-primary btn-sm")
+            button.setAttribute("id","joinButton"+i)
             playlistsList = document.getElementById('playlistsList');
             playlistsList.appendChild(li);
             //anchor.appendChild(li);
-            // var ul = document.getElementById("list"); 
-            // var li = document.createElement("li"); 
-            // li.appendChild(document.createTextNode("Four")); 
-            // var button = document.createElement("button"); 
-            // button.innerHTML = "asdasd"; 
-            // li.appendChild(button); 
-            // li.setAttribute("id","element4"); 
+            // var ul = document.getElementById("list");
+            // var li = document.createElement("li");
+            // li.appendChild(document.createTextNode("Four"));
+            // var button = document.createElement("button");
+            // button.innerHTML = "asdasd";
+            // li.appendChild(button);
+            // li.setAttribute("id","element4");
             // ul.appendChild(li); alert(li.id);
            }
-        }, 
+           for(let p=0; p < keys.length; p++){
+            var h=document.getElementById("joinButton"+p)
+             h.onclick=function(){
+                var k = keys[p];
+                var URI = playlists[k].URI;
+                var SpotifyID = playlists[k].SpotifyID;
+                var newurl = playlists[k].Urlforlist;
+                var spotifyToken= playlists[k].SpotifyToken;
+               srcloc= "https://open.spotify.com/embed?uri=spotify:user:"+ SpotifyID +":playlist:"+URI;
+              //window.location='playlist_template.html';
+              localStorage.setItem('SpotId',SpotifyID)
+              console.log(SpotifyID)
+              localStorage.setItem('URI',URI)
+              console.log(URI)
+              localStorage.setItem('URL',newurl)
+              console.log(newurl)
+              localStorage.setItem('spotifyToken',spotifyToken)
+              console.log(spotifyToken)
+               document.getElementById('list').src = srcloc;
+               console.log(srcloc)
+             }
+           }
+        },
         function (error) {
            console.log("Error: " + error.code);
         });
@@ -158,4 +179,3 @@ firebase.auth().onAuthStateChanged(user => {
 });
 
 }());
-
